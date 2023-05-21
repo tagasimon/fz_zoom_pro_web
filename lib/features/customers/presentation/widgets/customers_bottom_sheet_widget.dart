5fcfc468601443a4b50d92fa8d7755cb5893a827
Kazooba Simon <smkabz@gmail.com>
2023-05-21 14:52:06 +0300

    view customers insights

--- lib/features/customers/presentation/widgets/customers_bottom_sheet_widget.dart
+++ lib/features/customers/presentation/widgets/customers_bottom_sheet_widget.dart
@@ -10,6 +10,10 @@ class CustomersBottomSheetWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final scrollController = ScrollController();
+    if (scrollController.hasClients) {
+      scrollController.jumpTo(50.0);
+    }
     return CallbackShortcuts(
       bindings: <ShortcutActivator, VoidCallback>{
         const SingleActivator(LogicalKeyboardKey.escape): () =>
@@ -21,74 +25,75 @@ class CustomersBottomSheetWidget extends ConsumerWidget {
           children: [
             Expanded(
               flex: 1,
-              child: Stack(children: [
-                customersMapWidget(customers: customers),
-                // Positioned(
-                //   // bottom left
-                //   bottom: 0,
-                //   left: 0,
-                //   child: Column(
-                //     crossAxisAlignment: CrossAxisAlignment.start,
-                //     children: [
-                //       Card(
-                //         child: Row(
-                //           crossAxisAlignment: CrossAxisAlignment.start,
-                //           mainAxisAlignment: MainAxisAlignment.start,
-                //           children: [
-                //             DataTable(
-                //               showBottomBorder: true,
-                //               columns: const [
-                //                 DataColumn(label: Text('KPI')),
-                //                 DataColumn(label: Text('#')),
-                //               ],
-                //               rows: const [
-                //                 DataRow(cells: [
-                //                   DataCell(Text('Total Visits')),
-                //                   DataCell(Text('0')),
-                //                 ]),
-                //                 DataRow(cells: [
-                //                   DataCell(Text('Distict Customers')),
-                //                   DataCell(Text('0')),
-                //                 ]),
-                //               ],
-                //             ),
-                //             const VerticalDivider(),
-                //             DataTable(
-                //               showBottomBorder: true,
-                //               columns: const [
-                //                 DataColumn(label: Text('AGENT')),
-                //                 DataColumn(label: Text('#VISITS')),
-                //               ],
-                //               rows: const [
-                //                 // for (var r in distictAgents)
-                //                 //   DataRow(cells: [
-                //                 //     DataCell(Text(r)),
-                //                 //     DataCell(Text(
-                //                 //         '${VisitAdherenceUtils.totalNumberOfVisitsByAgent(data: userVisits, agent: r)}')),
-                //                 //   ]),
-                //               ],
-                //             ),
-                //             const VerticalDivider(),
-                //           ],
-                //         ),
-                //       )
-                //     ],
-                //   ),
-                // ),
-              ]),
+              child: customersMapWidget(customers: customers),
+              // Stack(
+              //   children: [
+              //     // Positioned(
+              //     //   // bottom left
+              //     //   bottom: 0,
+              //     //   left: 0,
+              //     //   child: Column(
+              //     //     crossAxisAlignment: CrossAxisAlignment.start,
+              //     //     children: [
+              //     //       Card(
+              //     //         child: Row(
+              //     //           crossAxisAlignment: CrossAxisAlignment.start,
+              //     //           mainAxisAlignment: MainAxisAlignment.start,
+              //     //           children: [
+              //     //             DataTable(
+              //     //               showBottomBorder: true,
+              //     //               columns: const [
+              //     //                 DataColumn(label: Text('KPI')),
+              //     //                 DataColumn(label: Text('#')),
+              //     //               ],
+              //     //               rows: const [
+              //     //                 DataRow(cells: [
+              //     //                   DataCell(Text('Total Visits')),
+              //     //                   DataCell(Text('0')),
+              //     //                 ]),
+              //     //                 DataRow(cells: [
+              //     //                   DataCell(Text('Distict Customers')),
+              //     //                   DataCell(Text('0')),
+              //     //                 ]),
+              //     //               ],
+              //     //             ),
+              //     //             const VerticalDivider(),
+              //     //             DataTable(
+              //     //               showBottomBorder: true,
+              //     //               columns: const [
+              //     //                 DataColumn(label: Text('AGENT')),
+              //     //                 DataColumn(label: Text('#VISITS')),
+              //     //               ],
+              //     //               rows: const [
+              //     //                 // for (var r in distictAgents)
+              //     //                 //   DataRow(cells: [
+              //     //                 //     DataCell(Text(r)),
+              //     //                 //     DataCell(Text(
+              //     //                 //         '${VisitAdherenceUtils.totalNumberOfVisitsByAgent(data: userVisits, agent: r)}')),
+              //     //                 //   ]),
+              //     //               ],
+              //     //             ),
+              //     //             const VerticalDivider(),
+              //     //           ],
+              //     //         ),
+              //     //       )
+              //     //     ],
+              //     //   ),
+              //     // ),
+              //   ],
+              // ),
             ),
             const VerticalDivider(thickness: 1, width: 1),
             Expanded(
               flex: 1,
               child: Scrollbar(
                 thumbVisibility: true,
+                controller: scrollController,
                 child: SingleChildScrollView(
+                  controller: scrollController,
                   child: Column(
                     children: [
                       SfDonutChart(
-                        // bgColor: Theme.of(context).primaryColor,
-                        // barColor: Theme.of(context).primaryColor,
-                        // xlabColor: Colors.white,
                         chartData: CustomerChartUtils.customersPerDistrict(
                             customers: customers),
                         // isInverted: false,
@@ -97,9 +102,6 @@ class CustomersBottomSheetWidget extends ConsumerWidget {
                         // height: 400,
                       ),
                       SfPieChart(
-                        // bgColor: Theme.of(context).primaryColor,
-                        // barColor: Theme.of(context).primaryColor,
-                        // xlabColor: Colors.white,
                         chartData: CustomerChartUtils.customersPerBusinessType(
                             customers: customers),
                         // isInverted: false,
@@ -108,9 +110,6 @@ class CustomersBottomSheetWidget extends ConsumerWidget {
                         // height: 400,
                       ),
                       SfBarChart(
-                        bgColor: Colors.white,
-                        barColor: Theme.of(context).primaryColor,
-                        xlabColor: Theme.of(context).primaryColor,
                         chartData: CustomerChartUtils.customersPerRoute(
                             customers: customers),
                         isInverted: false,

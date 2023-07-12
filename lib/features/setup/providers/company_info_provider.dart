81740377805edfc313554c3384ad6c778193523e
Kazooba Simon <smkabz@gmail.com>
2023-07-12 21:31:14 +0300

    more refactoringg

---
+++ lib/features/setup/providers/company_info_provider.dart
@@ -0,0 +1,15 @@
+import 'package:field_zoom_pro_web/core/notifiers/session_notifier.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fz_hooks/fz_hooks.dart';
+
+final companyInfoRepositoryProvider = Provider<CompanyInfoRepository>((ref) {
+  return CompanyInfoRepository(ref.watch(firestoreInstanceProvider));
+});
+
+final companyInfoStreamProvider =
+    StreamProvider.autoDispose<CompanyModel>((ref) {
+  final companyId = ref.watch(sessionNotifierProvider).loggedInUser!.companyId;
+  return ref
+      .watch(companyInfoRepositoryProvider)
+      .getCompanyInfo(companyId: companyId);
+});
